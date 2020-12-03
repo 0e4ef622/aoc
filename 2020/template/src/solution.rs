ccfc89f6be2f9194a2989e4216d90b165a0425c0
Matthew Tran <0e4ef622@gmail.com>
2020-12-02 23:15:52 -0600

    Update template

--- 2020/template/src/solution.rs
+++ 2020/template/src/solution.rs
@@ -1,6 +1,8 @@
 use std::collections::*;
 use rand::random;
+use serde_scan::scan as s;
 pub fn part1(input: &str) -> impl std::fmt::Display {
+    0
 }
 
 pub fn part2(input: &str) -> impl std::fmt::Display {
