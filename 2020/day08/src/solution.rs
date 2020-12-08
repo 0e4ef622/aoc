bc9a97dfdcdb9178ed8ed256665b0e8d2f56d1c5
Matthew Tran <0e4ef622@gmail.com>
2020-12-08 01:44:06 -0600

    day 8 graph approach

--- 2020/day08/src/solution.rs
+++ 2020/day08/src/solution.rs
@@ -1,6 +1,3 @@
-use std::collections::*;
-use rand::random;
-use serde_scan::scan as s;
 pub fn part1(input: &str) -> impl std::fmt::Display {
     let mut lines = input.lines().map(|x| (false, x)).collect::<Vec<_>>();
     let mut acc = 0;
@@ -18,7 +15,62 @@ pub fn part1(input: &str) -> impl std::fmt::Display {
     }
 }
 
+fn parse_num(s: &[u8]) -> isize {
+    let mut r = 0;
+    for b in &s[1..] {
+        r = 10*r + (b - b'0') as isize;
+    }
+    if s[0] == b'-' {
+        -r
+    } else {
+        r
+    }
+}
+
+// graph based approach
 pub fn part2(input: &str) -> impl std::fmt::Display {
+    let input = input.as_bytes();
+    let lines = input.split(|&x| x == b'\n').filter(|&x| !x.is_empty());
+    let len = lines.clone().count();
+    let instructions = lines.map(|x| (&x[..3], parse_num(&x[4..])));
+
+    // accumulator, offset1, offset2, visited
+    let mut graph = vec![(0, 1, None, false); len*2];
+    // left half represents the program pre-modified, right half post-modified
+    let (lh, rh) = graph.split_at_mut(len);
+    for (i, (op, val)) in instructions.enumerate() {
+        match op[0] {
+            b'a' => {
+                lh[i].0 = val;
+                rh[i].0 = val;
+            }
+            b'j' => {
+                lh[i].1 = val;
+                rh[i].1 = val;
+                lh[i].2 = Some(len as isize + 1);
+            }
+            b'n' => lh[i].2 = Some(len as isize + val),
+            _ => unreachable!(),
+        }
+    }
+
+    // depth first search
+    let mut stack = vec![(0, 0isize)];
+    while let Some((mut acc, pos)) = stack.pop() {
+        if pos as usize == 2*len { return acc; }
+        let (accc, offset1, offset2, visited) = &mut graph[pos as usize];
+        if *visited { continue; }
+        *visited = true;
+        acc += *accc;
+        stack.push((acc, pos + *offset1));
+        if let Some(offset2) = *offset2 {
+            stack.push((acc, pos + offset2));
+        }
+    }
+    panic!("answer not found")
+}
+
+pub fn oldpart2(input: &str) -> impl std::fmt::Display {
     let mut lines = input.lines().map(|x| (false, x)).collect::<Vec<_>>();
     for k in 0..lines.len() {
         let mut acc = 0;
