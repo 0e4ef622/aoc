6e9d10cf32759744d2d2e302ce2bd5ea82a317f4
Matthew Tran <0e4ef622@gmail.com>
2020-12-17 00:21:03 -0600

    make day 17 better

--- 2020/day17/src/solution.rs
+++ 2020/day17/src/solution.rs
@@ -2,24 +2,24 @@ use std::collections::*;
 use rand::random;
 use serde_scan::scan as s;
 pub fn part1(input: &str) -> impl std::fmt::Display {
-    let mut cubes = HashMap::new();
+    let mut cubes = HashSet::new();
     for (y, line) in input.lines().enumerate() {
         for (x, c) in line.chars().enumerate() {
-            cubes.insert((x as isize, y as isize, 0), c == '#');
+            if c == '#' {
+                cubes.insert((x as isize, y as isize, 0));
+            }
         }
     }
 
     for _ in 0..6 {
         it(&mut cubes);
     }
-    cubes.values().filter(|x| **x).count()
+    cubes.len()
 }
 
-fn it(cubes: &mut HashMap<(isize, isize, isize), bool>) {
+fn it(cubes: &mut HashSet<(isize, isize, isize)>) {
     let mut adj_cnt = HashMap::<_,usize>::new();
-    for c in &*cubes {
-        let (cd, a) = c;
-        if !a { continue; }
+    for cd in &*cubes {
         for x in -1..=1 {
             for y in -1..=1 {
                 for z in -1..=1 {
@@ -30,70 +30,64 @@ fn it(cubes: &mut HashMap<(isize, isize, isize), bool>) {
         }
     }
 
-    let mut new = HashMap::new();
+    let mut new = Vec::new();
 
     for cnt in adj_cnt {
         let (cd, cnt) = cnt;
-        if !cubes.get(&cd).unwrap_or(&false) {
+        if !cubes.contains(&cd) {
             if cnt == 3 {
-                new.insert(cd, true);
-            } else {
-                new.insert(cd, false);
+                new.push(cd);
             }
         } else if cnt == 2 || cnt == 3 {
-            new.insert(cd, true);
-        } else {
-            new.insert(cd, false);
+            new.push(cd);
         }
     }
-    *cubes = new;
+    cubes.clear();
+    cubes.extend(new);
 }
 
-fn it2(cubes: &mut HashMap<(isize, isize, isize, isize), bool>) {
+fn it2(cubes: &mut HashSet<(isize, isize, isize, isize)>) {
     let mut adj_cnt = HashMap::<_,usize>::new();
-    for c in &*cubes {
-        let (cd, a) = c;
-        if !a { continue; }
+    for cd in &*cubes {
         for x in -1..=1 {
             for y in -1..=1 {
                 for z in -1..=1 {
                     for w in -1..=1 {
-                    if x == 0 && y == 0 && z == 0 && w == 0 { continue; }
-                    *adj_cnt.entry((x+cd.0, y+cd.1, z+cd.2, w + cd.3)).or_default() += 1;
+                        if x == 0 && y == 0 && z == 0 && w == 0 { continue; }
+                        *adj_cnt.entry((x+cd.0, y+cd.1, z+cd.2, w+cd.3)).or_default() += 1;
                     }
                 }
             }
         }
     }
 
-    let mut new = HashMap::new();
+    let mut new = Vec::new();
 
     for cnt in adj_cnt {
         let (cd, cnt) = cnt;
-        if !cubes.get(&cd).unwrap_or(&false) {
+        if !cubes.contains(&cd) {
             if cnt == 3 {
-                new.insert(cd, true);
-            } else {
-                new.insert(cd, false);
+                new.push(cd);
             }
         } else if cnt == 2 || cnt == 3 {
-            new.insert(cd, true);
-        } else {
-            new.insert(cd, false);
+            new.push(cd);
         }
     }
-    *cubes = new;
+    cubes.clear();
+    cubes.extend(new);
 }
 pub fn part2(input: &str) -> impl std::fmt::Display {
-    let mut cubes = HashMap::new();
+    let mut cubes = HashSet::new();
     for (y, line) in input.lines().enumerate() {
         for (x, c) in line.chars().enumerate() {
-            cubes.insert((x as isize, y as isize, 0isize,0isize), c == '#');
+            if c == '#' {
+                cubes.insert((x as isize, y as isize, 0, 0));
+            }
         }
     }
 
     for _ in 0..6 {
         it2(&mut cubes);
     }
-    cubes.values().filter(|x| **x).count()
+    cubes.len()
 }
