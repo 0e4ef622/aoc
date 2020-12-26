362d67daf47f029f3208c155e2a9bed525353533
Matthew Tran <0e4ef622@gmail.com>
2020-12-26 13:26:26 -0600

    update template

--- 2020/template/src/solution.rs
+++ 2020/template/src/solution.rs
@@ -1,6 +1,17 @@
 use std::collections::*;
 use rand::random;
 use serde_scan::scan as s;
+
+trait ApplyTo: Sized {
+    fn app<F, T>(self, f: F) -> T
+    where
+        F: FnOnce(Self) -> T,
+    {
+        f(self)
+    }
+}
+impl<T> ApplyTo for T {}
+
 pub fn part1(input: &str) -> impl std::fmt::Display {
     0
 }
