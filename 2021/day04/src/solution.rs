182e606e99c50bcaa273c25547377d903b4d57d6
Matthew Tran <0e4ef622@gmail.com>
2021-12-03 23:31:07 -0600

    improve day 4

--- 2021/day04/src/solution.rs
+++ 2021/day04/src/solution.rs
@@ -1,53 +1,30 @@
-use std::collections::*;
 use rand::random;
 use serde_scan::scan as s;
+use std::collections::*;
 use util::*;
 
 fn has_col(b: &[[bool; 5]; 5]) -> bool {
-    for i in 0..5 {
-        let mut ok = true;
-        for j in 0..5 {
-            ok &= b[j][i];
-        }
-        if ok { return true; }
-    }
-    false
+    (0..5).any(|i| (0..5).all(|j| b[j][i]))
 }
 fn has_row(b: &[[bool; 5]; 5]) -> bool {
-    for j in 0..5 {
-        let mut ok = true;
-        for i in 0..5 {
-            ok &= b[j][i];
-        }
-        if ok { return true; }
-    }
-    false
+    b.iter().any(|v| v.iter().all(|x| *x))
 }
 
 fn win(b: &[[bool; 5]; 5]) -> bool {
     has_col(b) || has_row(b)
 }
 
-fn set(b: &mut Vec<Vec<&str>>, t: &mut [[bool; 5]; 5], v: &str) {
-    for i in 0..5 {
-        for j in 0..5 {
-            if b[i][j] == v {
-                t[i][j] = true;
-            }
-        }
-    }
+fn set<'a>(b: &mut Vec<Vec<&'a str>>, t: &mut [[bool; 5]; 5], v: &'a str) {
+    (0..5)
+        .flat_map(|i| (0..5).map(move |j| (i, j)))
+        .for_each(|(i, j)| t[i][j] |= b[i][j] == v);
 }
 
 fn score(b: &Vec<Vec<&str>>, t: &[[bool; 5]; 5]) -> i64 {
-    let mut un = 0;
-    for i in 0..5 {
-        for j in 0..5 {
-            if !t[i][j] {
-                un += b[i][j].parse::<i64>().unwrap();
-            }
-        }
-    }
-    un
+    (0..5)
+        .flat_map(|i| (0..5).map(move |j| (i, j)))
+        .map(|(i, j)| !t[i][j] as i64 * b[i][j].parse::<i64>().unwrap())
+        .sum()
 }
 
 pub fn part1(input: &str) -> impl std::fmt::Display {
@@ -88,7 +65,9 @@ pub fn part2(input: &str) -> impl std::fmt::Display {
         for (j, (b, t)) in board.iter_mut().zip(&mut t).enumerate() {
             let w = win(t);
             set(b, t, n);
-            if win(t) && !w { i = j; }
+            if win(t) && !w {
+                i = j;
+            }
         }
     }
     score(&board[i], &t[i]) * n.parse::<i64>().unwrap()
