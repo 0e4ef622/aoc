3e1a51d38d59d62976a0e4eaf9174778b5e96a13
Matthew Tran <0e4ef622@gmail.com>
2021-12-04 23:48:55 -0600

    day 5

---
+++ 2021/day05/src/solution.rs
@@ -0,0 +1,55 @@
+use std::collections::*;
+use rand::random;
+use serde_scan::scan as s;
+use util::*;
+
+pub fn part1(input: &str) -> impl std::fmt::Display {
+    let mut map = HashMap::new();
+    for line in input.lines() {
+        let mut s = line.split(" -> ");
+        let mut a = s.next().unwrap().split(",");
+        let mut b = s.next().unwrap().split(",");
+
+        let mut ax = a.next().unwrap().parse::<i64>().unwrap();
+        let mut ay = a.next().unwrap().parse::<i64>().unwrap();
+        let bx = b.next().unwrap().parse::<i64>().unwrap();
+        let by = b.next().unwrap().parse::<i64>().unwrap();
+        if !(ax == bx || ay == by) { continue; }
+        let mut dx = bx - ax;
+        let mut dy = by - ay;
+        if dx != 0 { dx /= dx.abs(); }
+        if dy != 0 { dy /= dy.abs(); }
+        *map.entry((ax, ay)).or_insert(0) += 1;
+        while ax != bx || ay != by {
+            ax += dx;
+            ay += dy;
+            *map.entry((ax, ay)).or_insert(0) += 1;
+        }
+    }
+    map.values().filter(|x| **x>1).count()
+}
+
+pub fn part2(input: &str) -> impl std::fmt::Display {
+    let mut map = HashMap::new();
+    for line in input.lines() {
+        let mut s = line.split(" -> ");
+        let mut a = s.next().unwrap().split(",");
+        let mut b = s.next().unwrap().split(",");
+
+        let mut ax = a.next().unwrap().parse::<i64>().unwrap();
+        let mut ay = a.next().unwrap().parse::<i64>().unwrap();
+        let bx = b.next().unwrap().parse::<i64>().unwrap();
+        let by = b.next().unwrap().parse::<i64>().unwrap();
+        let mut dx = bx - ax;
+        let mut dy = by - ay;
+        if dx != 0 { dx /= dx.abs(); }
+        if dy != 0 { dy /= dy.abs(); }
+        *map.entry((ax, ay)).or_insert(0) += 1;
+        while ax != bx || ay != by {
+            ax += dx;
+            ay += dy;
+            *map.entry((ax, ay)).or_insert(0) += 1;
+        }
+    }
+    map.values().filter(|x| **x>1).count()
+}
