3910b9f8f30b12bc0eeac3067b35ac2a7b4439c5
Matthew Tran <0e4ef622@gmail.com>
2021-12-06 23:20:41 -0600

    improve day 7

--- 2021/day07/src/solution.rs
+++ 2021/day07/src/solution.rs
@@ -13,17 +13,15 @@ pub fn part2(input: &str) -> impl std::fmt::Display {
     let mut n = input.trim().split(',').map(|x| x.parse::<i64>().unwrap()).cv();
     n.sort();
     let s = n.iter().sum::<i64>();
-    let m = (s as f64 / (n.len() as f64)).round() as i64;
-    n.iter().map(|v| {
-        let d = (v-m).abs();
-        d*(d+1)/2
-    }).sum::<i64>().min(
-    n.iter().map(|v| {
-        let d = (v-(m+1)).abs();
-        d*(d+1)/2
-    }).sum::<i64>()).min(
-    n.iter().map(|v| {
-        let d = (v-(m-1)).abs();
-        d*(d+1)/2
-    }).sum::<i64>())
+    let mm = s as f64 / (n.len() as f64);
+    let m = mm.floor() as i64;
+    let v0 = n.iter()
+        .map(|v| (v-m).abs())
+        .map(|v| v*(v+1)/2)
+        .sum::<i64>();
+    let v1 = n.iter()
+        .map(|v| (v-m-1).abs())
+        .map(|v| v*(v+1)/2)
+        .sum::<i64>();
+    v0.min(v1)
 }
