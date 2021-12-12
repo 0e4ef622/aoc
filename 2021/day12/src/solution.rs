a7ff1bd6b7432fd615e7ca15e2e1daa468cd2085
Matthew Tran <0e4ef622@gmail.com>
2021-12-12 12:12:23 -0600

    cleanup day 12

--- 2021/day12/src/solution.rs
+++ 2021/day12/src/solution.rs
@@ -1,50 +1,63 @@
-use std::collections::*;
-use rand::random;
 use itertools::{iproduct, Itertools};
+use rand::random;
+use std::collections::*;
 use util::*;
 
-fn dfs<'a>(g: &HashMap<&'a str, Vec<&'a str>>, v: &mut HashMap<&'a str, u8>, mut wtf: bool, i: &'a str) -> usize {
-    if i == "end" { return 1; }
-    if i.to_lowercase() == i {
-        let e = v.entry(i).or_default();
-        if *e >= 1 {
-            if wtf || i == "start" { return 0; }
-            wtf = true;
+fn is_lower(current: &str) -> bool {
+    current.bytes().all(|c| (b'a'..=b'z').contains(&c))
+}
+
+fn dfs<'a>(
+    graph: &HashMap<&'a str, Vec<&'a str>>,
+    visited: &mut HashMap<&'a str, u8>,
+    mut double: bool,
+    current: &'a str,
+) -> usize {
+    if current == "end" {
+        return 1;
+    }
+    if is_lower(current) {
+        let v_cnt = visited.entry(current).or_default();
+        if *v_cnt >= 1 {
+            if double || current == "start" {
+                return 0;
+            }
+            double = true;
         }
-        *e += 1;
+        *v_cnt += 1;
     }
-    let mut c = 0;
-    for &ch in g.get(i).unwrap_or(&vec![]) {
-        c += dfs(g, v, wtf, ch);
+    let mut paths = 0;
+    for &neighbor in graph.get(current).unwrap_or(&vec![]) {
+        paths += dfs(graph, visited, double, neighbor);
     }
 
-    if let Some(v) = v.get_mut(i) { *v -= 1; }
+    if let Some(v_cnt) = visited.get_mut(current) {
+        *v_cnt -= 1;
+    }
 
-    c
+    paths
 }
 
 pub fn part1(input: &str) -> impl std::fmt::Display {
     let mut g = HashMap::new();
     for line in input.lines() {
-        let w = line.split('-').cv();
-        let a = w[0];
-        let b = w[1];
+        let mut w = line.split('-');
+        let a = w.next().unwrap();
+        let b = w.next().unwrap();
         g.entry(a).or_insert(vec![]).push(b);
         g.entry(b).or_insert(vec![]).push(a);
     }
-    let mut v = HashMap::new();
-    dfs(&mut g, &mut v, true, "start")
+    dfs(&mut g, &mut Default::default(), true, "start")
 }
 
 pub fn part2(input: &str) -> impl std::fmt::Display {
     let mut g = HashMap::new();
     for line in input.lines() {
-        let w = line.split('-').cv();
-        let a = w[0];
-        let b = w[1];
+        let mut w = line.split('-');
+        let a = w.next().unwrap();
+        let b = w.next().unwrap();
         g.entry(a).or_insert(vec![]).push(b);
         g.entry(b).or_insert(vec![]).push(a);
     }
-    let mut v = HashMap::new();
-    dfs(&mut g, &mut v, false, "start")
+    dfs(&mut g, &mut Default::default(), false, "start")
 }
