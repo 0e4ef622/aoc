e71f38394d0bbd0186022bd476026bbb8a171f73
Matthew Tran <0e4ef622@gmail.com>
2021-12-12 13:36:00 -0600

    optimize day 12

--- 2021/day12/src/solution.rs
+++ 2021/day12/src/solution.rs
@@ -8,56 +8,81 @@ fn is_lower(current: &str) -> bool {
 }
 
 fn dfs<'a>(
-    graph: &HashMap<&'a str, Vec<&'a str>>,
-    visited: &mut HashMap<&'a str, u8>,
+    graph: &Vec<Vec<u8>>,
+    mut visited: u32,
     mut double: bool,
-    current: &'a str,
+    current: u8,
 ) -> usize {
-    if current == "end" {
+    if current == 2 {
         return 1;
     }
-    if is_lower(current) {
-        let v_cnt = visited.entry(current).or_default();
-        if *v_cnt >= 1 {
-            if double || current == "start" {
+    if current % 2 == 0 {
+        if visited & (1 << current) != 0 {
+            if double || current == 0 {
                 return 0;
             }
             double = true;
         }
-        *v_cnt += 1;
+        visited |= 1 << current;
     }
     let mut paths = 0;
-    for &neighbor in graph.get(current).unwrap_or(&vec![]) {
+    for &neighbor in &graph[current as usize] {
         paths += dfs(graph, visited, double, neighbor);
-    }
-
-    if let Some(v_cnt) = visited.get_mut(current) {
-        *v_cnt -= 1;
     }
 
     paths
 }
 
 pub fn part1(input: &str) -> impl std::fmt::Display {
-    let mut g = HashMap::new();
+    // Even IDs represent small caves, odd represent large.
+    let mut id_map = HashMap::new();
+    id_map.insert("start", 0);
+    id_map.insert("end", 2);
+    let mut g: Vec<Vec<u8>> = vec![vec![]; 3];
+    let mut x = 2;
+    let mut id = |g: &mut Vec<_>, s| {
+        match id_map.entry(s) {
+            hash_map::Entry::Occupied(e) => *e.get(),
+            hash_map::Entry::Vacant(e) => {
+                x += (x % 2 != is_lower(s) as u8) as u8 + 1;
+                g.resize((x+1) as usize, vec![]);
+                *e.insert(x)
+            }
+        }
+    };
     for line in input.lines() {
         let mut w = line.split('-');
-        let a = w.next().unwrap();
-        let b = w.next().unwrap();
-        g.entry(a).or_insert(vec![]).push(b);
-        g.entry(b).or_insert(vec![]).push(a);
+        let a = id(&mut g, w.next().unwrap());
+        let b = id(&mut g, w.next().unwrap());
+        g[a as usize].push(b);
+        g[b as usize].push(a);
     }
-    dfs(&mut g, &mut Default::default(), true, "start")
+    dfs(&mut g, 0, true, 0)
 }
 
 pub fn part2(input: &str) -> impl std::fmt::Display {
-    let mut g = HashMap::new();
+    // Even IDs represent small caves, odd represent large.
+    let mut id_map = HashMap::new();
+    id_map.insert("start", 0);
+    id_map.insert("end", 2);
+    let mut g: Vec<Vec<u8>> = vec![vec![]; 3];
+    let mut x = 2;
+    let mut id = |g: &mut Vec<_>, s| {
+        match id_map.entry(s) {
+            hash_map::Entry::Occupied(e) => *e.get(),
+            hash_map::Entry::Vacant(e) => {
+                x += (x % 2 != is_lower(s) as u8) as u8 + 1;
+                g.resize((x+1) as usize, vec![]);
+                *e.insert(x)
+            }
+        }
+    };
     for line in input.lines() {
         let mut w = line.split('-');
-        let a = w.next().unwrap();
-        let b = w.next().unwrap();
-        g.entry(a).or_insert(vec![]).push(b);
-        g.entry(b).or_insert(vec![]).push(a);
+        let a = id(&mut g, w.next().unwrap());
+        let b = id(&mut g, w.next().unwrap());
+        g[a as usize].push(b);
+        g[b as usize].push(a);
     }
-    dfs(&mut g, &mut Default::default(), false, "start")
+    dfs(&mut g, 0, false, 0)
 }
