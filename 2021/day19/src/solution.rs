9f81ccccd92da1b032a21d1d423676978cdd7808
Matthew Tran <0e4ef622@gmail.com>
2021-12-19 12:57:50 -0600

    optimize day 19 more

--- 2021/day19/src/solution.rs
+++ 2021/day19/src/solution.rs
@@ -1,6 +1,6 @@
-use std::collections::*;
-use rand::random;
 use itertools::{iproduct, Itertools};
+use rand::random;
+use std::collections::*;
 use util::*;
 
 type HashSet<T> = std::collections::HashSet<T, ahash::RandomState>;
@@ -66,7 +66,7 @@ impl<'a> Rots<'a> {
 fn merge(a: &[[i16; 3]], b: &[[i16; 3]]) -> Option<[i16; 3]> {
     for (aa, bb) in iproduct!(a, b) {
         let d = [aa[0] - bb[0], aa[1] - bb[1], aa[2] - bb[2]];
-        let bset = b.iter().map(|c| [c[0]+d[0], c[1]+d[1], c[2]+d[2]]);
+        let bset = b.iter().map(|c| [c[0] + d[0], c[1] + d[1], c[2] + d[2]]);
         if bset.filter(|v| a.contains(v)).count() >= 12 {
             return Some(d);
         }
@@ -79,7 +79,11 @@ fn rotmerge(a: &[[i16; 3]], b: &mut [[i16; 3]], calc: bool) -> Option<[i16; 3]> 
     while rots.rot() {
         if let Some(d) = merge(a, rots.a) {
             if calc {
-                for v in b { v[0] += d[0]; v[1] += d[1]; v[2] += d[2]; }
+                for v in b {
+                    v[0] += d[0];
+                    v[1] += d[1];
+                    v[2] += d[2];
+                }
             }
             return Some(d);
         }
@@ -94,87 +98,174 @@ fn c3<T>(mut i: impl Iterator<Item = T>) -> [T; 3] {
 fn idx2<T>(s: &mut [T], i: usize, j: usize) -> [&mut T; 2] {
     debug_assert_ne!(i, j);
     let ptr = s.as_mut_ptr();
-    unsafe {
-        [&mut *ptr.offset(i as isize), &mut *ptr.offset(j as isize)]
+    unsafe { [&mut *ptr.offset(i as isize), &mut *ptr.offset(j as isize)] }
+}
+
+fn dist(a: [i16; 3], b: [i16; 3]) -> i64 {
+    let (x, y, z) = (
+        a[0] as i64 - b[0] as i64,
+        a[1] as i64 - b[1] as i64,
+        a[2] as i64 - b[2] as i64,
+    );
+    x * x + y * y + z * z
+}
+
+fn maybe_overlap(a: &[[i16; 3]], b: &[[i16; 3]]) -> Option<[Vec<[i16; 3]>; 2]> {
+    #[derive(Eq)]
+    struct Dist {
+        dist: i64,
+        p1: [i16; 3],
+        p2: [i16; 3],
     }
-}
 
-fn dist(a: [i16; 3], b: [i16; 3]) -> u64 {
-    let (x, y, z) = (a[0] as u64-b[0] as u64, a[1] as u64-b[1] as u64, a[2] as u64-b[2] as u64);
-    x*x + y*y + z*z
-}
+    impl PartialEq for Dist {
+        fn eq(&self, other: &Self) -> bool {
+            self.dist == other.dist
+        }
+    }
+    impl std::hash::Hash for Dist {
+        fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+            self.dist.hash(state);
+        }
+    }
 
-fn maybe_overlap(a: &[[i16; 3]], b: &[[i16; 3]]) -> bool {
     let mut ads = HashSet::default();
     for i in 0..a.len() {
-        for j in i+1..a.len() {
-            ads.insert(dist(a[i], a[j]));
+        for j in i + 1..a.len() {
+            ads.insert(Dist {
+                dist: dist(a[i], a[j]),
+                p1: a[i],
+                p2: a[j],
+            });
         }
     }
+
     let mut bds = HashSet::default();
     for i in 0..b.len() {
-        for j in i+1..b.len() {
-            bds.insert(dist(b[i], b[j]));
+        for j in i + 1..b.len() {
+            bds.insert(Dist {
+                dist: dist(b[i], b[j]),
+                p1: b[i],
+                p2: b[j],
+            });
         }
     }
-    ads.intersection(&bds).count() >= 12*11/2
+    let intersection = ads.intersection(&bds).cv();
+    if intersection.len() < 12 * 11 / 2 {
+        return None;
+    }
+    let mut ads = intersection.iter().flat_map(|v| [v.p1, v.p2]).cv();
+    let mut bds = intersection
+        .into_iter()
+        .flat_map(|e| bds.get(e))
+        .flat_map(|v| [v.p1, v.p2])
+        .cv();
+
+    ads.sort_unstable();
+    ads.dedup();
+    bds.sort_unstable();
+    bds.dedup();
+
+    if ads.len() < 12 || bds.len() < 12 {
+        return None;
+    }
+
+    Some([ads, bds])
 }
 
-fn dfs(sc: &mut [Vec<[i16; 3]>], s: &mut Vec<[i16; 3]>, f: &mut HashSet<[i16; 3]>, g: &[Vec<usize>], i: usize, p: usize) {
-    let [a, b] = idx2(sc, i, p);
-    s.push(rotmerge(b, a, true).unwrap());
-    f.extend(sc[i].iter().copied());
-    for &ch in &g[i] {
-        if ch == p { continue; }
-        dfs(sc, s, f, g, ch, i);
+fn dfs(
+    scanners: &mut [Vec<[i16; 3]>],
+    scanner_pos: &mut Vec<[i16; 3]>,
+    beacons: &mut HashSet<[i16; 3]>,
+    graph: &[Vec<usize>],
+    current: usize,
+    previous: usize,
+) {
+    if current != previous {
+        let [a, b] = idx2(scanners, current, previous);
+        scanner_pos.push(rotmerge(b, a, true).unwrap());
     }
+    beacons.extend(scanners[current].iter().copied());
+    for &ch in &graph[current] {
+        if ch == previous {
+            continue;
+        }
+        dfs(scanners, scanner_pos, beacons, graph, ch, current);
+    }
+}
+
+fn parse_input(input: &str) -> Vec<Vec<[i16; 3]>> {
+    let mut sc = input
+        .split("\n\n")
+        .map(|sect| {
+            sect.lines()
+                .skip(1)
+                .map(|s| c3(s.split(',').map(|v| v.parse::<i16>().unwrap())))
+                .cv()
+                .chd(<[_]>::sort_unstable)
+        })
+        .cv();
+    sc
 }
 
 pub fn part1(input: &str) -> impl std::fmt::Display {
-    let mut sc = input.split("\n\n").map(|sect| sect.lines().skip(1).map(|s| c3(s.split(',').map(|v| v.parse::<i16>().unwrap()))).cv().chd(<[_]>::sort_unstable)).cv();
-    let mut g = vec![vec![]; sc.len()];
-    let mut s = Dsu::new(sc.len());
-    let mut cn = sc.len()-1;
-    'a: for i in 0..sc.len() {
-        for j in i+1..sc.len() {
-            if s.find(i) == s.find(j) { continue; }
-            if !maybe_overlap(&sc[i], &sc[j]) { continue; }
-            let [a, b] = idx2(&mut sc, i, j);
-            if let Some(_o) = rotmerge(a, b, false) {
-                cn -= 1;
-                s.merge(i, j);
-                g[i].push(j);
-                g[j].push(i);
-                if cn == 0 { break; }
+    let mut scanners = parse_input(input);
+    let mut graph = vec![vec![]; scanners.len()];
+    let mut dsu = Dsu::new(scanners.len());
+    let mut edges_left = scanners.len() - 1;
+    for i in 0..scanners.len() {
+        for j in i + 1..scanners.len() {
+            if dsu.find(i) == dsu.find(j) {
+                continue;
+            }
+            if let Some([a, mut b]) = maybe_overlap(&scanners[i], &scanners[j]) {
+                if let Some(_o) = rotmerge(&a, &mut b, false) {
+                    edges_left -= 1;
+                    // eprintln!("{} remaining", cn);
+                    dsu.merge(i, j);
+                    graph[i].push(j);
+                    graph[j].push(i);
+                    if edges_left == 0 {
+                        break;
+                    }
+                }
             }
         }
     }
     let mut f = HashSet::default();
-    dfs(&mut sc, &mut vec![], &mut f, &g, 0, 0);
+    dfs(&mut scanners, &mut vec![], &mut f, &graph, 0, 0);
     f.len()
 }
 
 pub fn part2(input: &str) -> impl std::fmt::Display {
-    let mut sc = input.split("\n\n").map(|sect| sect.lines().skip(1).map(|s| c3(s.split(',').map(|v| v.parse::<i16>().unwrap()))).cv().chd(<[_]>::sort_unstable)).cv();
-    let mut g = vec![vec![]; sc.len()];
-    let mut s = Dsu::new(sc.len());
-    let mut cn = sc.len()-1;
-    'a: for i in 0..sc.len() {
-        for j in i+1..sc.len() {
-            if s.find(i) == s.find(j) { continue; }
-            if !maybe_overlap(&sc[i], &sc[j]) { continue; }
-            let [a, b] = idx2(&mut sc, i, j);
-            if let Some(_o) = rotmerge(a, b, false) {
-                cn -= 1;
-                s.merge(i, j);
-                g[i].push(j);
-                g[j].push(i);
-                if cn == 0 { break; }
+    let mut scanners = parse_input(input);
+    let mut graph = vec![vec![]; scanners.len()];
+    let mut dsu = Dsu::new(scanners.len());
+    let mut edges_left = scanners.len() - 1;
+    for i in 0..scanners.len() {
+        for j in i + 1..scanners.len() {
+            if dsu.find(i) == dsu.find(j) {
+                continue;
+            }
+            if let Some([a, mut b]) = maybe_overlap(&scanners[i], &scanners[j]) {
+                if let Some(_o) = rotmerge(&a, &mut b, false) {
+                    edges_left -= 1;
+                    // eprintln!("{} remaining", cn);
+                    dsu.merge(i, j);
+                    graph[i].push(j);
+                    graph[j].push(i);
+                    if edges_left == 0 {
+                        break;
+                    }
+                }
             }
         }
     }
     let mut f = HashSet::default();
     let mut sp = vec![];
-    dfs(&mut sc, &mut sp, &mut f, &g, 0, 0);
-    iproduct!(&sp, &sp).map(|(a, b)| (a[0]-b[0]).abs() + (a[1]-b[1]).abs() + (a[2]-b[2]).abs()).max().unwrap()
+    dfs(&mut scanners, &mut sp, &mut f, &graph, 0, 0);
+    iproduct!(&sp, &sp)
+        .map(|(a, b)| (a[0] - b[0]).abs() + (a[1] - b[1]).abs() + (a[2] - b[2]).abs())
+        .max()
+        .unwrap()
 }
