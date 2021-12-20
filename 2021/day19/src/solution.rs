309c332d596cef24d78683f1717b46b69c3f8da1
Matthew Tran <0e4ef622@gmail.com>
2021-12-19 20:48:18 -0600

    optimize day 19 more

--- 2021/day19/src/solution.rs
+++ 2021/day19/src/solution.rs
@@ -5,64 +5,92 @@ use util::*;
 
 type HashSet<T> = std::collections::HashSet<T, ahash::RandomState>;
 
-fn rot(a: &mut [[i16; 3]], dir: &str) {
-    match dir {
-        "xy" => a.iter_mut().for_each(|v| (v[0], v[1]) = (v[1], -v[0])),
-        "yz" => a.iter_mut().for_each(|v| (v[1], v[2]) = (v[2], -v[1])),
-        "-yz" => a.iter_mut().for_each(|v| (v[1], v[2]) = (-v[2], v[1])),
-        "xz" => a.iter_mut().for_each(|v| (v[0], v[2]) = (v[2], -v[0])),
-        _ => todo!(),
+fn rot(p: [i16; 3], r: [i16; 3]) -> [i16; 3] {
+    [
+        r[0].signum() * p[r[0].abs() as usize - 1],
+        r[1].signum() * p[r[1].abs() as usize - 1],
+        r[2].signum() * p[r[2].abs() as usize - 1],
+    ]
+}
+
+fn invrot(r: [i16; 3]) -> [i16; 3] {
+    let mut r2 = [0; 3];
+    for i in 0..3 { r2[r[i].abs() as usize - 1] = r[i].signum() * (i + 1) as i16; }
+    r2
+}
+
+fn unrot(p: [i16; 3], r: [i16; 3]) -> [i16; 3] {
+    rot(p, invrot(r))
+}
+
+// Rotate, then translate
+#[derive(Copy, Clone, Debug)]
+struct Transform {
+    rot: [i16; 3],
+    trans: [i16; 3],
+}
+
+impl Transform {
+    fn new() -> Self {
+        Self { rot: [1, 2, 3], trans: [0; 3] }
+    }
+    fn then(&self, o: &Transform) -> Self {
+        let mut trans = rot(self.trans, o.rot);
+        trans[0] += o.trans[0];
+        trans[1] += o.trans[1];
+        trans[2] += o.trans[2];
+        let rot = rot(self.rot, o.rot);
+        Self { rot, trans }
+    }
+    fn inv(&self) -> Self {
+        let mut trans = unrot(self.trans, self.rot);
+        trans[0] *= -1;
+        trans[1] *= -1;
+        trans[2] *= -1;
+        let rot = invrot(self.rot);
+        Self { trans, rot }
+    }
+
+    fn apply(&self, mut p: [i16; 3]) -> [i16; 3] {
+        p = rot(p, self.rot);
+        p[0] += self.trans[0];
+        p[1] += self.trans[1];
+        p[2] += self.trans[2];
+        p
     }
 }
 
-struct Rots<'a> {
-    a: &'a mut [[i16; 3]],
-    c: u8,
-}
+static ROTS: [[i16; 3]; 24] = [
+    [1, 2, 3],
+    [2, -1, 3],
+    [2, -1, 3],
+    [2, -1, 3],
 
-impl<'a> Rots<'a> {
-    fn new(a: &'a mut [[i16; 3]]) -> Self {
-        Self { a, c: 0 }
-    }
+    [3, 2, -1],
+    [2, -1, 3],
+    [2, -1, 3],
+    [2, -1, 3],
 
-    fn rot(&mut self) -> bool {
-        self.c += 1;
-        match self.c {
-            1 => (),
-            2 => rot(self.a, "xy"),
-            3 => rot(self.a, "xy"),
-            4 => rot(self.a, "xy"),
+    [3, 2, -1],
+    [2, -1, 3],
+    [2, -1, 3],
+    [2, -1, 3],
 
-            5 => rot(self.a, "xz"),
-            6 => rot(self.a, "xy"),
-            7 => rot(self.a, "xy"),
-            8 => rot(self.a, "xy"),
+    [1, 3, -2],
+    [2, -1, 3],
+    [2, -1, 3],
+    [2, -1, 3],
 
-            9 => rot(self.a, "xz"),
-            10 => rot(self.a, "xy"),
-            11 => rot(self.a, "xy"),
-            12 => rot(self.a, "xy"),
+    [1, -3, 2],
+    [2, -1, 3],
+    [2, -1, 3],
+    [2, -1, 3],
 
-            13 => rot(self.a, "yz"),
-            14 => rot(self.a, "xy"),
-            15 => rot(self.a, "xy"),
-            16 => rot(self.a, "xy"),
-
-            17 => rot(self.a, "-yz"),
-            18 => rot(self.a, "xy"),
-            19 => rot(self.a, "xy"),
-            20 => rot(self.a, "xy"),
-
-            21 => rot(self.a, "-yz"),
-            22 => rot(self.a, "xy"),
-            23 => rot(self.a, "xy"),
-            24 => rot(self.a, "xy"),
-            _ => return false,
-        }
-        true
-    }
-}
-
+    [1, -3, 2],
+    [2, -1, 3],
+    [2, -1, 3],
+    [2, -1, 3],
+];
 fn merge(a: &[[i16; 3]], b: &[[i16; 3]]) -> Option<[i16; 3]> {
     for (aa, bb) in iproduct!(a, b) {
         let d = [aa[0] - bb[0], aa[1] - bb[1], aa[2] - bb[2]];
@@ -74,18 +102,16 @@ fn merge(a: &[[i16; 3]], b: &[[i16; 3]]) -> Option<[i16; 3]> {
     None
 }
 
-fn rotmerge(a: &[[i16; 3]], b: &mut [[i16; 3]], calc: bool) -> Option<[i16; 3]> {
-    let mut rots = Rots::new(b);
-    while rots.rot() {
-        if let Some(d) = merge(a, rots.a) {
-            if calc {
-                for v in b {
-                    v[0] += d[0];
-                    v[1] += d[1];
-                    v[2] += d[2];
-                }
-            }
-            return Some(d);
+fn rotmerge(a: &[[i16; 3]], b: &mut [[i16; 3]]) -> Option<Transform> {
+    let mut ro = [1, 2, 3];
+    for &r in &ROTS {
+        ro = rot(ro, r);
+        b.iter_mut().for_each(|p| *p = rot(*p, r));
+        if let Some(d) = merge(a, b) {
+            let mut t = Transform { rot: [1,2,3], trans: d };
+            b.iter_mut().for_each(|p| *p = t.apply(*p));
+            t.rot = ro;
+            return Some(t);
         }
     }
     None
@@ -154,7 +180,10 @@ fn maybe_overlap(a: &[[i16; 3]], b: &[[i16; 3]]) -> Option<[Vec<[i16; 3]>; 2]> {
     if intersection.len() < 12 * 11 / 2 {
         return None;
     }
-    let mut ads = intersection.iter().flat_map(|v| [v.p1, v.p2]).cv();
+    let mut ads = intersection
+        .iter()
+        .flat_map(|e| ads.get(e))
+        .flat_map(|v| [v.p1, v.p2]).cv();
     let mut bds = intersection
         .into_iter()
         .flat_map(|e| bds.get(e))
@@ -177,20 +206,21 @@ fn dfs(
     scanners: &mut [Vec<[i16; 3]>],
     scanner_pos: &mut Vec<[i16; 3]>,
     beacons: &mut HashSet<[i16; 3]>,
-    graph: &[Vec<usize>],
+    graph: &[Vec<(usize, Transform, bool)>],
     current: usize,
     previous: usize,
+    t: Transform,
 ) {
+    scanner_pos.push(t.trans);
     if current != previous {
-        let [a, b] = idx2(scanners, current, previous);
-        scanner_pos.push(rotmerge(b, a, true).unwrap());
+        scanners[current].iter_mut().for_each(|p| *p = t.apply(*p));
     }
     beacons.extend(scanners[current].iter().copied());
-    for &ch in &graph[current] {
+    for &(ch, tt, ck) in &graph[current] {
         if ch == previous {
             continue;
         }
-        dfs(scanners, scanner_pos, beacons, graph, ch, current);
+        dfs(scanners, scanner_pos, beacons, graph, ch, current, tt.then(&t));
     }
 }
 
@@ -219,12 +249,12 @@ pub fn part1(input: &str) -> impl std::fmt::Display {
                 continue;
             }
             if let Some([a, mut b]) = maybe_overlap(&scanners[i], &scanners[j]) {
-                if let Some(_o) = rotmerge(&a, &mut b, false) {
+                if let Some(t) = rotmerge(&a, &mut b) {
                     edges_left -= 1;
                     // eprintln!("{} remaining", cn);
                     dsu.merge(i, j);
-                    graph[i].push(j);
-                    graph[j].push(i);
+                    graph[i].push((j, t, true));
+                    graph[j].push((i, t.inv(), false));
                     if edges_left == 0 {
                         break;
                     }
@@ -233,7 +263,7 @@ pub fn part1(input: &str) -> impl std::fmt::Display {
         }
     }
     let mut f = HashSet::default();
-    dfs(&mut scanners, &mut vec![], &mut f, &graph, 0, 0);
+    dfs(&mut scanners, &mut vec![], &mut f, &graph, 0, 0, Transform::new());
     f.len()
 }
 
@@ -248,12 +278,12 @@ pub fn part2(input: &str) -> impl std::fmt::Display {
                 continue;
             }
             if let Some([a, mut b]) = maybe_overlap(&scanners[i], &scanners[j]) {
-                if let Some(_o) = rotmerge(&a, &mut b, false) {
+                if let Some(t) = rotmerge(&a, &mut b) {
                     edges_left -= 1;
                     // eprintln!("{} remaining", cn);
                     dsu.merge(i, j);
-                    graph[i].push(j);
-                    graph[j].push(i);
+                    graph[i].push((j, t, true));
+                    graph[j].push((i, t.inv(), false));
                     if edges_left == 0 {
                         break;
                     }
@@ -263,7 +293,7 @@ pub fn part2(input: &str) -> impl std::fmt::Display {
     }
     let mut f = HashSet::default();
     let mut sp = vec![];
-    dfs(&mut scanners, &mut sp, &mut f, &graph, 0, 0);
+    dfs(&mut scanners, &mut sp, &mut f, &graph, 0, 0, Transform::new());
     iproduct!(&sp, &sp)
         .map(|(a, b)| (a[0] - b[0]).abs() + (a[1] - b[1]).abs() + (a[2] - b[2]).abs())
         .max()
