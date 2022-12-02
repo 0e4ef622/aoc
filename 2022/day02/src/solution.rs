a74aba60eb010103f4b92faacbb3dfdc159265eb
Matthew Tran <0e4ef622@gmail.com>
2022-12-02 02:27:22 -0600

    formatting

--- 2022/day02/src/solution.rs
+++ 2022/day02/src/solution.rs
@@ -1,6 +1,6 @@
-use std::collections::*;
-use rand::random;
 use itertools::{iproduct, Itertools};
+use rand::random;
+use std::collections::*;
 use util::*;
 
 pub fn part1(input: &str) -> impl std::fmt::Display {
@@ -11,10 +11,10 @@ pub fn part1(input: &str) -> impl std::fmt::Display {
         let x = wh.next().unwrap();
         let a = a.chars().next().unwrap() as i64 - 'A' as i64;
         let x = x.chars().next().unwrap() as i64 - 'X' as i64;
-        sc += x+1;
+        sc += x + 1;
         if a == x {
             sc += 3;
-        } else if x == (3 + a+1)%3 {
+        } else if x == (a + 1) % 3 {
             sc += 6;
         }
     }
@@ -29,9 +29,9 @@ pub fn part2(input: &str) -> impl std::fmt::Display {
         let x = wh.next().unwrap();
         let a = a.chars().next().unwrap() as i64 - 'A' as i64;
         let x = match x {
-            "X" => sc += (a+2)%3+1,
-            "Y" => sc += 3 + a%3+1,
-            "Z" => sc += (a+1)%3 + 6+1,
+            "X" => sc += (a + 2) % 3 + 1,
+            "Y" => sc += 3 + a + 1,
+            "Z" => sc += (a + 1) % 3 + 6 + 1,
             _ => unreachable!(),
         };
     }
