cdc4b9f345a3ed6dbf66d40d2584b98bcb9c3492
Matthew Tran <0e4ef622@gmail.com>
2022-12-17 23:42:50 -0600

    optimize day 17

--- 2022/day18/src/solution.rs
+++ 2022/day18/src/solution.rs
@@ -25,6 +25,9 @@ pub fn part2(input: &str) -> impl std::fmt::Display {
         let mut it = line.split(',').map(|x| x.parse::<i64>().unwrap());
         (it.next().unwrap(), it.next().unwrap(), it.next().unwrap())
     }).collect::<HashSet<_>>();
+    let maxx = cubes.iter().map(|c| c.0).max().unwrap()+2;
+    let maxy = cubes.iter().map(|c| c.1).max().unwrap()+2;
+    let maxz = cubes.iter().map(|c| c.2).max().unwrap()+2;
     let mut sfa = 0;
     for &(x,y,z) in &cubes {
         if !cubes.contains(&(x+1,y,z)) { sfa+=1; }
@@ -34,26 +37,28 @@ pub fn part2(input: &str) -> impl std::fmt::Display {
         if !cubes.contains(&(x,y,z+1)) { sfa+=1; }
         if !cubes.contains(&(x,y,z-1)) { sfa+=1; }
     }
-    let mut inner = 100*100*100;
     let mut queue = VecDeque::new();
-    queue.push_back((0,0,0));
-    cubes.insert((0,0,0));
+    queue.push_back((-1,-1,-1));
+    cubes.insert((-1,-1,-1));
     while let Some((x,y,z)) = queue.pop_front() {
-        inner-=1;
         for (dx,dy,dz) in [(1,0,0),(-1,0,0),(0,1,0),(0,-1,0),(0,0,1),(0,0,-1)] {
-            if !cubes.contains(&(x+dx,y+dy,z+dz)) && x+dx < 100 && x+dx >= 0
-                && y+dy < 100 && y+dy >= 0
-                && z+dz < 100 && z+dz >= 0
+            let nx = x+dx;
+            let ny = y+dy;
+            let nz = z+dz;
+            if !cubes.contains(&(nx, ny, nz))
+                && (-1..maxx).contains(&nx)
+                && (-1..maxy).contains(&ny)
+                && (-1..maxz).contains(&nz)
             {
-                cubes.insert((x+dx,y+dy,z+dz));
-                queue.push_back((x+dx,y+dy,z+dz));
+                cubes.insert((nx,ny,nz));
+                queue.push_back((nx,ny,nz));
             }
         }
     }
     let mut notcubes = HashSet::new();
-    for x in 0..100 {
-        for y in 0..100 {
-            for z in 0..100 {
+    for x in -1..maxx {
+        for y in -1..maxy {
+            for z in -1..maxz {
                 if !cubes.contains(&(x,y,z)) {
                     notcubes.insert((x,y,z));
                 }
