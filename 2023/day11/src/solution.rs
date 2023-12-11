d053fd10e7c4eb14c134120fd3aaca4e3538c0cf
Matthew Tran <0e4ef622@gmail.com>
2023-12-11 02:07:40 -0600

    day 11 micro optimize

--- 2023/day11/src/solution.rs
+++ 2023/day11/src/solution.rs
@@ -1,44 +1,33 @@
 use std::collections::*;
-use rand::random;
 use itertools::{iproduct, Itertools};
 use util::*;
 
-pub fn solve(input: &str, expansion: usize) -> i64 {
+pub fn solve(input: &str, expansion: i64) -> i64 {
     let g = input.lines().map(|x| x.as_bytes()).cv();
-    let mut rows = Vec::with_capacity(g.len());
-    let mut cols = Vec::with_capacity(g[0].len());
-    for (i, l) in g.iter().enumerate() {
-        let mut has_g = l.contains(&b'#');
-        if !has_g {
-            rows.push(i);
-        }
-    }
-    for j in 0..g[0].len() {
-        let mut has_g = false;
-        for i in 0..g.len() {
-            if g[i][j] == b'#' {
-                has_g = true;
-                break;
-            }
-        }
-        if !has_g {
-            cols.push(j);
-        }
-    }
     let mut x = vec![];
     let mut y = vec![];
-    for i in 0..g.len() {
-        for j in 0..g[i].len() {
-            if g[i][j] == b'#' {
-                let ii = i + expansion*rows.iter().filter(|&&x| x < i).count();
-                let jj = j + expansion*cols.iter().filter(|&&x| x < j).count();
-                y.push(ii as i64);
-                x.push(jj as i64);
+    for (i, l) in g.iter().enumerate() {
+        for (j, c) in l.iter().enumerate() {
+            if *c == b'#' {
+                y.push(i as i64);
+                x.push(j as i64);
             }
         }
     }
     x.sort_unstable();
+    expand(&mut x, expansion);
+    expand(&mut y, expansion);
     sum_diff(&x) + sum_diff(&y)
+}
+
+fn expand(v: &mut [i64], f: i64) {
+    let mut p = 0;
+    let mut c = 0;
+    for x in &mut *v {
+        c += (*x - p - 1).max(0);
+        p = *x;
+        *x += c*f;
+    }
 }
 
 fn sum_diff(v: &[i64]) -> i64 {
