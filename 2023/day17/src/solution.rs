b72dd73ebb4e506aacd4196725643c8c345dc032
Matthew Tran <0e4ef622@gmail.com>
2023-12-17 00:35:26 -0600

    day 17 refactor shared code

--- 2023/day17/src/solution.rs
+++ 2023/day17/src/solution.rs
@@ -67,34 +67,7 @@ impl<T> CustomHeap<T> {
     }
 }
 
-pub fn part1(input: &str) -> impl std::fmt::Display {
-    let g = input.lines().map(str::as_bytes).cv();
-    let w = g[0].len() as i32;
-    let h = g.len() as i32;
-    let mut q = CustomHeap::new(2048);
-    q.push(0, (0i32, (0i32, 0i32), 0, R));
-    let mut v = vec![false; (w*h*4*4) as usize];
-    let bck = |(x,y)| x>=0 && x<w && y>=0 && y<h;
-    let mut ans = i32::MAX;
-    while let Some((mut d, (x, y), c, dir)) = q.pop() {
-        if v[(dir as i32 + 4*(c + 4*(x + w*y))) as usize] { continue; }
-        v[(dir as i32 + 4*(c + 4*(x + w*y))) as usize] = true;
-        d += (g[y as usize][x as usize] - b'0') as i32;
-        if x == w-1 && y == h-1 {
-            ans = d;
-            break;
-        }
-        let r = mv(x,y,right(dir));
-        let l = mv(x,y,left(dir));
-        let f = mv(x,y,dir);
-        if bck(r) && !v[(right(dir) as i32 + 4*(1 + 4*(r.0 + w*r.1))) as usize] { q.push(d, (d, r, 1, right(dir))); }
-        if bck(l) && !v[(left(dir) as i32 + 4*(1 + 4*(l.0 + w*l.1))) as usize] { q.push(d, (d, l, 1, left(dir))); }
-        if c < 3 && bck(f) && !v[(dir as i32 + 4*(c+1 + 4*(f.0 + w*f.1))) as usize] { q.push(d, (d, f, c+1, dir)); }
-    }
-    ans - (g[0][0] - b'0') as i32
-}
-
-pub fn part2(input: &str) -> impl std::fmt::Display {
+pub fn solve<const TURN_MIN: i32, const STRAIGHT_MAX: i32>(input: &str) -> i32 {
     let g = input.lines().map(str::as_bytes).cv();
     let w = g[0].len() as i32;
     let h = g.len() as i32;
@@ -115,10 +88,17 @@ pub fn part2(input: &str) -> impl std::fmt::Display {
         let r = mv(x,y,right(dir));
         let l = mv(x,y,left(dir));
         let f = mv(x,y,dir);
-        if c >= 4 && bck(r) && !v[(right(dir) as i32 + 4*(1 + 11*(r.0 + w*r.1))) as usize] { q.push(d, (d, r, 1, right(dir))); }
-        if c >= 4 && bck(l) && !v[(left(dir) as i32 + 4*(1 + 11*(l.0 + w*l.1))) as usize] { q.push(d, (d, l, 1, left(dir))); }
-        if c < 10 && bck(f) && !v[(dir as i32 + 4*(c+1 + 11*(f.0 + w*f.1))) as usize] { q.push(d, (d, f, c+1, dir)); }
+        if c >= TURN_MIN && bck(r) && !v[(right(dir) as i32 + 4*(1 + 11*(r.0 + w*r.1))) as usize] { q.push(d, (d, r, 1, right(dir))); }
+        if c >= TURN_MIN && bck(l) && !v[(left(dir) as i32 + 4*(1 + 11*(l.0 + w*l.1))) as usize] { q.push(d, (d, l, 1, left(dir))); }
+        if c < STRAIGHT_MAX && bck(f) && !v[(dir as i32 + 4*(c+1 + 11*(f.0 + w*f.1))) as usize] { q.push(d, (d, f, c+1, dir)); }
     }
     ans - (g[0][0] - b'0') as i32
 }
 
+pub fn part1(input: &str) -> impl std::fmt::Display {
+    solve::<0, 3>(input)
+}
+
+pub fn part2(input: &str) -> impl std::fmt::Display {
+    solve::<4, 10>(input)
+}
