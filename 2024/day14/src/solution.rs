92890a50c9b591e556742b9c117ff3b85d7d32d6
Matthew Tran <0e4ef622@gmail.com>
2024-12-13 23:58:37 -0600

    day 14

---
+++ 2024/day14/src/solution.rs
@@ -0,0 +1,72 @@
+use std::collections::*;
+use rand::random;
+use itertools::{iproduct, Itertools};
+use util::*;
+
+const W: i64 = 101;
+const H: i64 = 103;
+// const W: i64 = 11;
+// const H: i64 = 7;
+const W2: i64 = W/2;
+const H2: i64 = H/2;
+const W21: i64 = W/2+1;
+const H21: i64 = H/2+1;
+
+fn quadrant(p: P<i64>) -> usize {
+    match (p.0.rem_euclid(W), p.1.rem_euclid(H)) {
+        (0..W2, 0..H2) => 0,
+        (W21..W, 0..H2) => 1,
+        (W21..W, H21..H) => 2,
+        (0..W2, H21..H) => 3,
+        _ => 4,
+    }
+}
+
+pub fn part1(input: &str) -> impl std::fmt::Display {
+    let bots = input.lines().map(|l| {
+        let [px, py, _, _, vx, vy] = *l[2..].split([',', ' ', 'v', '=']).cv() else { unreachable!() };
+        (P(px.parse::<i64>().unwrap(), py.parse::<i64>().unwrap()), P(vx.parse::<i64>().unwrap(), vy.parse::<i64>().unwrap()))
+    }).cv();
+
+    let mut qc = [0; 5];
+    for b in bots {
+        let p = b.0 + 100*b.1;
+        qc[quadrant(p)] += 1;
+    }
+    let ans = qc[0] * qc[1] * qc[2] * qc[3];
+    assert_ne!(ans, 226650624);
+    ans
+}
+
+pub fn print(bots: &[(P<i64>, P<i64>)], t: i64) {
+    let mut g = [[false; W as usize]; H as usize];
+    for b in bots {
+        let p = b.0 + (t % (W*H))*b.1;
+        g[p.1.rem_euclid(H) as usize][p.0.rem_euclid(W) as usize] = true;
+    }
+    for r in g {
+        for c in r {
+            if c {
+                print!("#");
+            } else {
+                print!(".");
+            }
+        }
+        println!();
+    }
+}
+
+pub fn part2(input: &str) -> impl std::fmt::Display {
+    let bots = input.lines().map(|l| {
+        let [px, py, _, _, vx, vy] = *l[2..].split([',', ' ', 'v', '=']).cv() else { unreachable!() };
+        (P(px.parse::<i64>().unwrap(), py.parse::<i64>().unwrap()), P(vx.parse::<i64>().unwrap(), vy.parse::<i64>().unwrap()))
+    }).cv();
+
+    for i in 0..W*H {
+        println!("{i}");
+        print(&bots, i);
+        println!();
+        std::thread::sleep_ms(50);
+    }
+    0
+}
