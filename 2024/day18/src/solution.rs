64749bc57866c8bdc41babcbcab033ae19a29bea
Matthew Tran <0e4ef622@gmail.com>
2024-12-17 23:31:54 -0600

    day 17 union find

--- 2024/day18/src/solution.rs
+++ 2024/day18/src/solution.rs
@@ -32,44 +32,38 @@ pub fn part1(input: &str) -> impl std::fmt::Display {
     dist[end]
 }
 
-pub fn ppart1(input: &str, n: usize) -> i64 {
-    let coords = input.lines().map(|x| {
-        let [x, y] = *x.split(',').map(|n| n.parse::<usize>().unwrap()).cv() else { unreachable!() };
-        [x, y]
-    }).cv();
-    let mut g = Grid::new(b'.', 71, 71);
-
-    for &[x, y] in &coords[..n] {
-        g[P(x, y)] = b'#';
-    }
-
-    let mut dist = Grid::new(-1, g.width, g.height);
-    let start = P(0i64, 0);
-    let end = P(70i64, 70);
-
-    let mut q = BinaryHeap::new();
-    q.push(Reverse((0, start)));
-    while let Some(Reverse((cdist, p))) = q.pop() {
-        if dist[p] != -1 { continue; }
-        dist[p] = cdist;
-        for d in Dir::iter() {
-            if g.get(p+d) == Some(&b'.') && dist[p+d] == -1 {
-                q.push(Reverse((cdist+1, p+d)));
-            }
-        }
-    }
-    dist[end]
-}
-
 pub fn part2(input: &str) -> impl std::fmt::Display {
     let coords = input.lines().map(|x| {
         let [x, y] = *x.split(',').map(|n| n.parse::<usize>().unwrap()).cv() else { unreachable!() };
         [x, y]
     }).cv();
-    for i in 1024..coords.len() {
-        if ppart1(input, i) == -1 {
-            return format!("{},{}", coords[i-1][0], coords[i-1][1]);
+    let mut g = Grid::new(b'.', 71, 71);
+    for &[x, y] in &coords {
+        g[P(x, y)] = b'#';
+    }
+
+    let mut dsu = Dsu::new(71*71);
+    let m = |p: P<i64>| (p.1*71+p.0) as usize;
+    for (p, &c) in g.iter_coords() {
+        if c == b'#' { continue; }
+        for d in Dir::iter() {
+            if g.get(p+d) == Some(&b'.') {
+                dsu.merge(m(p), m(p+d));
+            }
         }
     }
-    "".into()
+
+    for (i, &[x, y]) in coords.iter().enumerate().rev() {
+        let p = P(x, y).ai();
+        g[P(x, y)] = b'.';
+        for d in Dir::iter() {
+            if g.get(p+d) == Some(&b'.') {
+                dsu.merge(m(p), m(p+d));
+            }
+        }
+        if dsu.find(0) == dsu.find(71*71-1) {
+            return format!("{x},{y}");
+        }
+    }
+    unreachable!();
 }
